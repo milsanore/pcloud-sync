f1cfc731acb907273deb111ba6daf15b57ba63df
mils <milsanore@gmail.com>
2023-04-01 12:46:00 +1100

    get new files first

--- sync.ps1
+++ sync.ps1
@@ -1,6 +1,6 @@
 $Path = "."
 
-$files = Get-ChildItem $Path -Recurse
+$files = Get-ChildItem $Path -Recurse | Sort-Object -Descending CreationTime
 foreach ($f in $files){
     Write-Host $f.FullName $f.Length
     Get-Content -Path $f.FullName | out-null
