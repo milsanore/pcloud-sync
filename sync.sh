06223034a03751754586225992ce922a2ef48302
milan <milsanore@gmail.com>
2024-01-31 21:51:07 +1100

    touch latest files first

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # enter the directory, run this command:
-for file in *; do cat "$file" > /dev/null; echo "$file"; done | pv -l -s "$(find . | wc -l)"
+ls -tU * | while read -r file; do cat "$file" > /dev/null; echo "$file"; done | pv -l -s "$(find . | wc -l)"
